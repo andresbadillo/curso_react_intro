a2dc40716a5f5fb8c1818be0eab338c10b6f3d5a
Andres Badillo <r.andres.badillo@gmail.com>
2023-05-17 14:15:46 -0500

    Clase 8 - comunicacion de estados

--- src/App.js
+++ src/App.js
@@ -6,18 +6,29 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
 const defaultTodos = [
-  { text: 'Responder a la SSPD', completed: false },
   { text: 'Estudiar inglés', completed: true },
-  { text: 'Enviar correo', completed: true },
   { text: 'Entrenar pierna', completed: false },
+  { text: 'Estudiar frances', completed: true },
+
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length; /* !! - devuelve un boleano */
+  const totalTodos = todos.length;
+
+  console.log(`Buscando TODOs de ${searchValue}`);
+
   return (
     <React.Fragment>
 
-      <TodoCounter completed={6} total={11}/>
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos}/>
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {defaultTodos.map(todo => (
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import './TodoSearch.css'
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState('');
-
-  console.log(`Buscando TODOs de ${searchValue}`);
-
+function TodoSearch({
+  searchValue, 
+  setSearchValue
+}) {
   return (
     <input 
       placeholder='Buscar TODO' 
