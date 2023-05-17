cb904d2b763756b67af8140526aaeb6be21d6053
Andres Badillo <r.andres.badillo@gmail.com>
2023-05-17 18:00:17 -0500

    Clase 12 - Iconos con colores dinamicos

---
+++ src/CompleteIcon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TodoIcon } from './TodoIcon'
+
+function CompleteIcon({ completed, onComplete }) {
+    return (
+        <TodoIcon 
+            type="check"
+            color={completed ? '#4caf50' : 'gray'}
+            onClick={onComplete}
+        />
+    );
+}
+
+export { CompleteIcon }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import './TodoItem.css'
+import React from 'react';
+import { CompleteIcon } from './CompleteIcon';
+import { DeleteIcon } from './DeleteIcon';
+import './TodoItem.css';
 
 function TodoItem(props) {
   return (
     <li className='TodoItem'>
-      <span 
-        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-        onClick={props.onComplete}
-      >V</span>
+
+      <CompleteIcon 
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
+
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-      <span 
-        className={`Icon Icon-delete`}
-        onClick={props.onDelete}
-      >X</span>
+
+      <DeleteIcon 
+        onDelete={props.onDelete}
+      />
+      
     </li>
   )
 }
