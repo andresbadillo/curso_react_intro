b4fb6a82ac29f04c70f683632eaafadb5a377c5a
Andres Badillo <r.andres.badillo@gmail.com>
2023-05-17 15:21:31 -0500

    Clase 10 - completando TODOs

--- src/App.js
+++ src/App.js
@@ -27,7 +27,23 @@ function App() {
     }
   );
 
-  console.log(`Buscando TODOs de ${searchValue}`);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <React.Fragment>
@@ -44,6 +60,8 @@ function App() {
             key={todo.text} 
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
+  // return (
+  //   <h1 className='TodoCounter'>
+  //     Has completado <span>{completed}</span> de <span>{total}</span> TODO's
+  //   </h1>
+  // )
   return (
-    <h1 className='TodoCounter'>
-      Has completado <span>{completed}</span> de <span>{total}</span> TODO's
-    </h1>
-  )
+    <>
+      {total === completed && (
+        <h1 className="TodoCounter">¡Felicidades, completaste todos las tareas!</h1>
+      )}
+      {total !== completed && (
+        <h1 className="TodoCounter">
+          Has completado <span>{completed}</span> de <span>{total}</span> TODO's
+        </h1>
+      )}
+    </>
+  );
 }
 
 export { TodoCounter };
