d12753049510c33b1ef4b95fcf753d0954c42036
Andres Badillo <r.andres.badillo@gmail.com>
2023-05-19 08:43:37 -0500

    Clase 17 - separacion UI en App

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
 import { useLocalStorage } from './useLocalStorage';
+import { AppUI } from './AppUI';
 
 // const defaultTodos = [
 //   { text: 'Estudiar inglés', completed: true },
@@ -51,29 +47,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-
-      <TodoCounter completed={completedTodos} total={totalTodos}/>
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-
-    </React.Fragment>
+    <AppUI 
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
